d70dc67fcee46954f63fe07cc1c832880b48cbb1
zzhaneka <zhanaibek.zhaniya@mail.ru>
2025-04-30 12:56:11 +0500

    Fix feeding logic, mood sync, and sleep animation

--- core/src/main/java/com/zhaniya/finalproject/ui/GameScreen.java
+++ core/src/main/java/com/zhaniya/finalproject/ui/GameScreen.java
@@ -1,10 +1,21 @@
 package com.zhaniya.finalproject.ui;
 
-import com.badlogic.gdx.*;
-import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.zhaniya.finalproject.model.pet.*;
+import com.zhaniya.finalproject.commands.FeedCommand;
+import com.zhaniya.finalproject.commands.PlayCommand;
+import com.zhaniya.finalproject.commands.SleepCommand;
+import com.zhaniya.finalproject.model.pet.Emotion;
+import com.zhaniya.finalproject.model.pet.Pet;
 
 import java.util.EnumMap;
 
@@ -18,6 +29,9 @@ public class GameScreen implements Screen {
     private BitmapFont font;
     private GlyphLayout layout;
 
+    private Stage stage;
+    private Texture feedTexture, playTexture, sleepTexture;
+
     public GameScreen(Game game, Pet pet) {
         this.game = game;
         this.pet = pet;
@@ -28,15 +42,12 @@ public class GameScreen implements Screen {
         batch = new SpriteBatch();
         emotionAnimations = new EnumMap<>(Emotion.class);
 
-        // 💬 Выбираем базовый путь в зависимости от типа питомца
-        String basePath = "";
-        switch (pet.getType()) {
-            case DRAGON -> basePath = "dragon";
-            case CAT -> basePath = "cat";
-            case DOG -> basePath = "dog";
-        }
+        String basePath = switch (pet.getType()) {
+            case DRAGON -> "dragon";
+            case CAT -> "cat";
+            case DOG -> "dog";
+        };
 
-        // 💬 Загружаем эмоции для выбранного питомца
         emotionAnimations.put(Emotion.HAPPY, loadAnimation(basePath + "/happy", 0.2f));
         emotionAnimations.put(Emotion.ANGRY, loadAnimation(basePath + "/angry", 0.2f));
         emotionAnimations.put(Emotion.SAD, loadAnimation(basePath + "/sad", 0.2f));
@@ -47,6 +58,59 @@ public class GameScreen implements Screen {
 
         font = new BitmapFont();
         layout = new GlyphLayout();
+
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        loadButtons();
+    }
+
+    private void loadButtons() {
+        // Загрузка текстур
+        feedTexture = new Texture("ui/buttons/feed_button.png");
+        playTexture = new Texture("ui/buttons/play_button.png");
+        sleepTexture = new Texture("ui/buttons/sleep_button.png");
+
+        // Кнопки
+        ImageButton feedButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(feedTexture)));
+        ImageButton playButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(playTexture)));
+        ImageButton sleepButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(sleepTexture)));
+
+        // Размер и позиция
+        feedButton.setSize(120, 50);
+        playButton.setSize(120, 50);
+        sleepButton.setSize(120, 50);
+
+        feedButton.setPosition(40, 30);
+        playButton.setPosition(180, 30);
+        sleepButton.setPosition(320, 30);
+
+        // Слушатели
+        feedButton.addListener(new ClickListener() {
+            @Override public void clicked(InputEvent event, float x, float y) {
+                new FeedCommand(pet).execute();
+                updateEmotion();
+            }
+        });
+
+        playButton.addListener(new ClickListener() {
+            @Override public void clicked(InputEvent event, float x, float y) {
+                new PlayCommand(pet).execute();
+                updateEmotion();
+            }
+        });
+
+        sleepButton.addListener(new ClickListener() {
+            @Override public void clicked(InputEvent event, float x, float y) {
+                new SleepCommand(pet).execute();
+                updateEmotion();
+            }
+        });
+
+        // Добавляем кнопки на сцену
+        stage.addActor(feedButton);
+        stage.addActor(playButton);
+        stage.addActor(sleepButton);
     }
 
     private Animation<TextureRegion> loadAnimation(String folderPath, float frameDuration) {
@@ -73,28 +137,44 @@ public class GameScreen implements Screen {
         TextureRegion currentFrame = animation.getKeyFrame(stateTime, true);
 
         batch.begin();
+        font.getData().setScale(2f);
+        layout.setText(font, "Your Pet");
+        font.draw(batch, layout, (Gdx.graphics.getWidth() - layout.width) / 2, 550);
+
         batch.draw(currentFrame, 150, 150, 200, 200);
 
         font.getData().setScale(1.5f);
-        String status = "Энергия: " + pet.getEnergy() + "\nНастроение: " + pet.getMood();
+        String status = "Energy: " + pet.getEnergy() + "\nMood: " + pet.getMood();
         layout.setText(font, status);
         font.draw(batch, layout, 20, 450);
+
         batch.end();
+
+        stage.act(delta);
+        stage.draw();
     }
 
-    @Override
-    public void resize(int width, int height) {}
-    @Override
-    public void pause() {}
-    @Override
-    public void resume() {}
-    @Override
-    public void hide() {}
+    public void updateEmotion() {
+        Emotion newEmotion = pet.getState().getEmotion();
+        if (newEmotion != currentEmotion) {
+            currentEmotion = newEmotion;
+            stateTime = 0f;
+        }
+    }
+
+    @Override public void resize(int width, int height) {}
+    @Override public void pause() {}
+    @Override public void resume() {}
+    @Override public void hide() {}
 
     @Override
     public void dispose() {
         batch.dispose();
         font.dispose();
+        stage.dispose();
+        feedTexture.dispose();
+        playTexture.dispose();
+        sleepTexture.dispose();
         for (Animation<TextureRegion> anim : emotionAnimations.values()) {
             for (TextureRegion region : anim.getKeyFrames()) {
                 region.getTexture().dispose();
