0d836ed7274a791786e0e57087511ebf04eb8c94
Moldiir <moldir.umrali@narxoz.kz>
2025-05-05 22:54:37 +0500

    updated timerUtil class

--- core/src/main/java/com/zhaniya/finalproject/utils/TimerUtil.java
+++ core/src/main/java/com/zhaniya/finalproject/utils/TimerUtil.java
@@ -25,11 +25,11 @@ public class TimerUtil {
                     System.out.println(pet.getName() + " не ел уже 2 минуты и теперь голоден!");
                 }
 
-                // Случайная болезнь — 5% шанс
-                if (Math.random() < 0.05 && !pet.isInState(SickState.class)) {
-                    pet.setState(new SickState(pet));
-                    System.out.println(pet.getName() + " внезапно заболел!");
-                }
+//                // Случайная болезнь — 5% шанс
+//                if (Math.random() < 0.05 && !pet.isInState(SickState.class)) {
+//                    pet.setState(new SickState(pet));
+//                    System.out.println(pet.getName() + " внезапно заболел!");
+//                }
             }
         }, 0, UPDATE_INTERVAL);
     }
